00a78fdbe61fd6a4aac2310daa0d551458bc0f50
Lesty612 <lesty612@163.com>
2019-11-29 17:57:14 +0800

    1. 升级vant库，修改对应组件样式 2. 忽略云函数目录下node_modules

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -9,7 +9,8 @@ App({
             console.error('请使用 2.2.3 或以上的基础库以使用云能力')
         } else {
             wx.cloud.init({
-                traceUser: true,
+                env: 'dev-364635',
+                traceUser: true
             })
         }
 
