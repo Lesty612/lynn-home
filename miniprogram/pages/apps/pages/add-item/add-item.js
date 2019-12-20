a5037610996babe31a5f834903600be266382079
Lesty612 <lesty612@163.com>
2019-12-20 18:17:17 +0800

    1. 完成新增物品 - 类别选择功能

--- miniprogram/pages/apps/pages/add-item/add-item.js
+++ miniprogram/pages/apps/pages/add-item/add-item.js
@@ -1,13 +1,10 @@
 const app = getApp();
 const Tools = app.Tools;
-
+const TYPE_LIST = ['短袖', '长袖', '外套', '长裤', '短裤', '裙子', '卫衣', '其它'];
+// 当前操作的cell name，用于唯一确认当前操作的popUp
+let curCellName;
 // pages/apps/pages/add-item/add-item.js
 Component({
-    /**
-     * 组件的属性列表
-     */
-    properties: {},
-
     /**
      * 组件的初始数据
      */
@@ -20,13 +17,42 @@ Component({
             price: '',
             score: '',
             season: ''
-        }
+        },
+        // 是否展示类别弹出框
+        isShowTypePopUp: false,
+        // 类别列表
+        typeList: TYPE_LIST
     },
 
     /**
      * 组件的方法列表
      */
     methods: {
+        // 处理cell点击
+        handCellClick({currentTarget}) {
+            let data = {};
+            curCellName = currentTarget.dataset.name;
+            data[`isShow${curCellName}PopUp`] = true;
+
+            this.setData(data);
+        },
+        // 处理picker确认
+        handlePickerConfirm({currentTarget, detail}) {
+            console.log(arguments);
+            let data = {};
+            data[`formData.${currentTarget.dataset.name}`] = detail.value;
+
+            this.setData(data);
+            this.handPopUpClose();
+        },
+        // 处理弹框关闭
+        handPopUpClose() {
+            let data = {};
+            data[`isShow${curCellName}PopUp`] = false;
+
+            this.setData(data);
+        },
+
         onShow() {
             console.log(Tools);
         }
